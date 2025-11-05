36219c60dc5518abffb03d03b7e85063d42c1444
SamuelGilvan7 <sankiragilvan@gmail.com>
2025-11-04 22:44:18 -0300

    Alterei as páginas
    Separação do menu de navegação em nav.php e inclusão na página index
    - Criação de arquivo nav.php com os links principais
    - Inclusão do menu em index.php usando require "nav.php"
    - Ajuste do formulário de login com method="post" e envio para valida_login.php

--- index.php
+++ index.php
@@ -16,12 +16,7 @@
 
   <body>
 
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        App Help Desk
-      </a>
-    </nav>
+    <?php require "nav.php" ?>
 
     <div class="container">    
       <div class="row">
@@ -32,33 +27,13 @@
               Login
             </div>
               <div class="card-body">
-            <form action="valida_login.php" method="get">
+            <form action="valida_login.php" method="post">
               <div class="form-group">
                 <input name="email" type="email" class="form-control" placeholder="E-mail">
               </div>
               <div class="form-group">
                 <input name="senha" type="password" class="form-control" placeholder="Senha">
               </div>
-
-              <?php
-                if (isset($_GET['login']) && $_GET['login'] == 'erro') { ?>
-                
-                
-              <div class="text-danger">
-                Usuário ou senha inválido!    
-              </div>
-             
-              <?php } ?>
-
-              <?php
-                if (isset($_GET['login']) && $_GET['login'] == 'erro2') { ?>
-                
-                
-              <div class="text-danger">
-                antes de acessar o sistema, é necessário fazer login!    
-              </div>
-             
-              <?php } ?>
               <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
             </form>
             </div>
