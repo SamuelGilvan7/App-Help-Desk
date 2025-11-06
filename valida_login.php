046e98836438aabcfa98b07468efa603593e65f0
SamuelGilvan7 <sankiragilvan@gmail.com>
2025-11-06 18:31:06 -0300

    Corrigiu erro de autenticação
    Implementa login em PHP com verificação e mensagem de erro ao errar credenciais.

--- valida_login.php
+++ valida_login.php
@@ -1,4 +1,27 @@
-<?php 
-    $_GET['email'];
-    $_GET['senha'];
+<?php
+    //variavel que verifica se autenticação foi realizada
+    $usuario_autenticado = false;
+
+    $usuario_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '12345'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcde'),
+    );
+
+    foreach($usuario_app as $user){
+        $user ['email'];
+        $user ['senha'];
+
+        if ($user ['email'] == $_POST['email'] && $user ['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;   
+        } 
+        if ($usuario_autenticado){
+            echo 'Usuário autenticado';
+        } else {
+            header('Location: index.php?login=erro'); //redireciona para a página de login e avisa que houve erro
+        }
+
+    }
+    
+    /*$_GET['email'];
+    $_GET['senha'];*/
 ?>
